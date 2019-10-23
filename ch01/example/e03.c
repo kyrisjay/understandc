d1c21b0f507ef15593be60a66ff64417c6276251
edz <edz@shatianjiedeMacBook-Air.local>
2019-10-23 18:17:52 +0800

    e03

---
+++ ch01/example/e03.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+ int main (void){
+ 	int inputNumber,inputNumberTwice;
+ 	
+ 	scanf("%d,%d",&inputNumber,&inputNumberTwice);
+ 	
+ 	printf("长方形的面积是%d\n",inputNumber * inputNumberTwice );
+	
+		return 0;
+
+ }
