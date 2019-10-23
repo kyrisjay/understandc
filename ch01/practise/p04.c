c0227a68e767fe1c346a4d79a923339fc248ac50
edz <edz@shatianjiedeMacBook-Air.local>
2019-10-23 20:44:26 +0800

    p04.c

--- ch01/practise/p04.c
+++ ch01/practise/p04.c
@@ -2,10 +2,10 @@
 int main(void){
 
 	int vx=3.14;
-	int vy=vx+5.7;
-	printf("vx的值是%d。\n",vx);
-	printf("vy的值是%d。\n",vy);
+	int vy=5.7;
+	printf("vx的值是%d\n",vx);
+	printf("vy的值是%d\n",vy);
 	return 0;
 
 }
-//报错不能够运行，int型变量赋值为整数，小数可以用float，占位符号为%f//
+//int类型只取整数//
